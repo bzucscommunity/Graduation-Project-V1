c862a99a9ef7eb51e295c9c80ee190283bdd6805
Osamajarrar <osamajarrar8@gmail.com>
2021-03-10 22:37:43 +0200

    CS Community

--- app/src/main/java/graduation/project/bzu/cscomunity/Activities/MainActivity.java
+++ app/src/main/java/graduation/project/bzu/cscomunity/Activities/MainActivity.java
@@ -6,8 +6,12 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ActionBar;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -27,14 +31,35 @@ import graduation.project.bzu.cscomunity.Adapters.GridSubjectsListAdapter;
 import graduation.project.bzu.cscomunity.DataModels.Subject;
 import graduation.project.bzu.cscomunity.R;
 
+import static android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     List<Subject> subjects;
     private String JSON_URL="http://192.168.1.111:8080/api/subject";
     GridSubjectsListAdapter adapter;
     @Override
+    protected void onResume(){
+        super.onResume();
+        View decorView = getWindow().getDecorView();
+// Hide both the navigation bar and the status bar.
+// SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+// a general rule, you should design your app to hide the status bar whenever you
+// hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+        decorView.setSystemUiVisibility(uiOptions);
+
+    }
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //getSupportActionBar().hide();
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+      //          WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.subjectsList);
         subjects=new ArrayList<>();
